78faadc3b5b47d45a209b43d72391e2c60418f51
cmoonth47 <cmoonth47@gmail.com>
2023-11-22 21:53:57 +0900

    11/22 bookshop 쇼핑몰 코드 분석

---
+++ bookShop01/src/main/java/com/bookshop01/common/file/FileDownloadController.java
@@ -0,0 +1,64 @@
+package com.bookshop01.common.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+
+@Controller
+public class FileDownloadController {
+	private static String CURR_IMAGE_REPO_PATH = "C:\\shopping\\file_repo";
+	
+	@RequestMapping("/download") // /download URL 매핑
+	// 'fileName' : 다운로드할 파일의 이름
+	// 'goods_id' : 상품의 식별자(폴더명)
+	// 'HttpServletResponse response' : 응답 헤더를 작성하고 파일 내용을 쓰는 데 사용.
+	protected void download(@RequestParam("fileName") String fileName,
+		                 	@RequestParam("goods_id") String goods_id,
+			                 HttpServletResponse response) throws Exception {
+		OutputStream out = response.getOutputStream();
+		String filePath=CURR_IMAGE_REPO_PATH+"\\"+goods_id+"\\"+fileName;
+		File image=new File(filePath);
+
+		response.setHeader("Cache-Control","no-cache");
+		response.addHeader("Content-disposition", "attachment; fileName="+fileName);
+		FileInputStream in=new FileInputStream(image); 
+		byte[] buffer=new byte[1024*8];
+		while(true){
+			int count=in.read(buffer); //���ۿ� �о���� ���ڰ���
+			if(count==-1)  //������ �������� �����ߴ��� üũ
+				break;
+			out.write(buffer,0,count);
+		}
+		in.close();
+		out.close();
+	}
+	
+	
+	@RequestMapping("/thumbnails.do")
+	protected void thumbnails(@RequestParam("fileName") String fileName,
+                            	@RequestParam("goods_id") String goods_id,
+			                 HttpServletResponse response) throws Exception {
+		OutputStream out = response.getOutputStream();
+		String filePath=CURR_IMAGE_REPO_PATH+"\\"+goods_id+"\\"+fileName;
+		File image=new File(filePath);
+		
+		if (image.exists()) { 
+			// 썸네일 생성 => 크기 지정, 출력 형식 지정, OutputStream(out) 썸네일
+			Thumbnails.of(image).size(121,154).outputFormat("png").toOutputStream(out);
+		}
+		byte[] buffer = new byte[1024 * 8];
+		out.write(buffer);
+		out.close();
+	}
+}
